3305ffd801af29392ea841f7251c56871e9d3b9c
Paul von Napolski <Paul_Beeblebrox42@gmx.de>
2022-12-21 13:23:19 +0100

    Add project files.

---
+++ Spam_program/Spam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Spam_progam {
+    class Spam {
+        public static void Main() {
+            int counter = 0;
+            string text = "#########";
+            for (int i = 0; i < 10; i++) {
+                text += text;
+            }
+            while (true) {
+                for (int i = 0; i < 1000; i++) {
+                    File.AppendAllText($"test{counter}.txt", text);
+                }
+                File.SetAttributes($"test{counter}.txt", FileAttributes.ReadOnly);
+                File.SetAttributes($"test{counter}.txt", FileAttributes.Hidden);
+                if (counter >= 5) {
+                    break;
+                }
+                counter++;
+            }
+        }
+    }
+}
